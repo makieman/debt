8f57745a8b7c5740549201f02aaa3391f3368280
makieman <markmaranga9@gmail.com>
2026-08-03 00:53:55 +0300

    SharedArrayBuffer

--- metro.config.js
+++ metro.config.js
@@ -17,4 +17,16 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.assetExts.push("wasm");
+
+config.server = {
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      return middleware(req, res, next);
+    };
+  },
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
